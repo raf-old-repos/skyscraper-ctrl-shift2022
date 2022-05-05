0bf150d35b55297c659db011dd53f868a20cdefc
Gitter499 <rafayel.amirkhanyan@gmail.com>
2022-05-05 11:17:39 +0000

    Added welcome scene

---
+++ src/scenes/WelcomeScene.tsx
@@ -0,0 +1,13 @@
+import { ScrollControls } from "@react-three/drei";
+
+type WelcomeSceneProps = {};
+
+const WelcomeScene: React.FC<WelcomeSceneProps> = (props) => {
+    return (
+        <ScrollControls damping={3} pages={} horizontal>
+            
+        </ScrollControls>
+    )
+}
+
+export default WelcomeScene;
